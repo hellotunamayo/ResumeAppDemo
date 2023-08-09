4d85b83e2227bbdddf2cf93bde2e75356762bbf5
hellotunamayo <yoo@minyoungyoo.com>
2023-08-10 08:25:25 +0900

    Initial Commit

---
+++ ResumeAppDemo/ResumeAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ResumeAppDemoApp.swift
+//  ResumeAppDemo
+//
+//  Created by Minyoung Yoo on 2023/08/10.
+//
+
+import SwiftUI
+
+@main
+struct ResumeAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
